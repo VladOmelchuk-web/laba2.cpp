515990cc1e2b4ddd0b550e4792d71be49c819b06
VladOmelchuk-web <63592115+VladOmelchuk-web@users.noreply.github.com>
2020-05-04 13:21:00 +0300

    Update laba2task3.cpp

--- laba2task3.cpp
+++ laba2task3.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+int DigitSum(int K);
+
+int main() {
+    for (int rep = 0; rep<5;rep++){
+        std::cout << "Enter K: ";
+        int k;
+        std::cin >> k;
+        std::cout << "Sum of digits: " << DigitSum(k) << endl;
+        system("pause");
+        system("cls");
+    }
+    return 0;
+}
+
+int DigitSum(int K) {
+    if (K < 0) K = -K;
+    if (K < 10) return K;
+    return K % 10 + DigitSum(K / 10);
+}
